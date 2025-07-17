e846827ea8e0812b97c14637b32d3885460100dd
sureshkumarbe <sureshkumarbecbe@gmail.com>
2025-07-17 16:33:52 +0530

    fourth commit

---
+++ app/src/main/java/com/airei/mypalm/mypalmplantation/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.airei.mypalm.mypalmplantation
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+
+    private val splashDuration: Long = 2000 // 2 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(mainLooper).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, splashDuration)
+    }
+}
